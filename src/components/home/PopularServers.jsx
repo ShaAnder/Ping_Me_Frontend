07d55b3ad8f04c3bdc6316fa582223645f7dbaf1
shaAnder <shaunanderton1@gmail.com>
2025-04-24 14:50:27 +0100

    redo home layout (final)

--- src/components/home/PopularServers.jsx
+++ src/components/home/PopularServers.jsx
@@ -3,6 +3,10 @@ import styles from "../../assets/css/popularServers.module.css";
 function PopularServers({ servers }) {
   return (
     <div className={styles.popularServersContainer}>
+      <div className={styles.textContainer}>
+        <h1>Popular Servers</h1>
+        <h3>Servers Talking about {servers[0].category}</h3>
+      </div>
       {servers.map((server) => (
         <div key={server.id} className={styles.serverCard}>
           <img
