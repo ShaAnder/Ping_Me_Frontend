0856b7e9775726cd993b5137c4e5a7219cafe5fb
shaAnder <shaunanderton1@gmail.com>
2025-05-18 20:05:28 +0100

    fix protected route error

--- src/services/ProtectedRoute.tsx
+++ src/services/ProtectedRoute.tsx
@@ -2,7 +2,14 @@ import { Navigate } from "react-router-dom";
 import { useUserAuth } from "../hooks/useUserAuth";
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const { isAuthenticated } = useUserAuth();
+  const { isAuthenticated, loading } = useUserAuth();
+
+  // Wait until loading is false before deciding
+  if (loading) {
+    // You can return a spinner or null
+    return null;
+  }
+
   if (!isAuthenticated) {
     return <Navigate to="/login" replace={true} />;
   }
